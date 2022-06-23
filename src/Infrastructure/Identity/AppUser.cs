8ca2c79c56ca14478c384be2bf5ab2a6c865e848
Berengaar <emir97han@gmail.com>
2022-06-23 16:53:09 +0300

    Microsoft identity models created

---
+++ src/Infrastructure/Identity/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
