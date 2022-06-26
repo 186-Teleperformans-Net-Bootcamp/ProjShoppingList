ceee2161d6259c84a09da56ead66b0288726e19a
Berengaar <emir97han@gmail.com>
2022-06-26 16:10:11 +0300

    IdentityService filled

---
+++ src/Domain/Consts/Messages/Identity/IdentityMessages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Consts.Messages.Identity
+{
+    public static class IdentityMessages
+    {
+        #region Errors
+        public static string EmailOrPasswordInvalid = "Email or password invalid";
+        public static string ExistEmail = "An account registered with this email already exists";
+        #endregion
+
+        #region Success
+        public static string UserCreated = "User created successfully.";
+        #endregion
+
+    }
+}
