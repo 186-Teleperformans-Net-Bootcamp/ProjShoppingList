2db4e5f8455b4ef7325c3084dbb4de756a27900f
Berengaar <emir97han@gmail.com>
2022-06-26 16:03:02 +0300

    IIdentityService added

---
+++ src/Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Models;
+using Application.Common.Models.Identity;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<(Result, string Token)> LoginAsync(LoginModel loginModel);
+        Task<Result> RegisterAsync(RegisterModel registerModel, UserRoles role = UserRoles.User);
+        Task<Result> RegisterAdminAsync(RegisterModel registerModel, UserRoles role = UserRoles.Admin);
+    }
+}
