80bbcbfa99258ca424dc0b69bb21fd4dcd4ca21f
Berengaar <emir97han@gmail.com>
2022-06-28 20:38:05 +0300

    Error fixed

--- src/Application/Common/Interfaces/IRabbitMqService.cs
+++ src/Application/Common/Interfaces/IRabbitMqService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entities.AdminEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,6 @@ namespace Application.Common.Interfaces
         public string UserName { get; set; }
         public string Password { get; set; }
 
-        void SendDataToQueue();
+        void SendDataToQueue(CompletedList list);
     }
 }
--- src/Infrastructure/RabbitMq/ProducerOperations.cs
+++ src/Infrastructure/RabbitMq/ProducerOperations.cs
@@ -51,5 +51,7 @@ namespace Infrastructure.RabbitMq
             UserName = configurationManager["RabbitMqOptions:UserName"];
             Password = configurationManager["RabbitMqOptions:Password"];
         }
+
+     
     }
 }
