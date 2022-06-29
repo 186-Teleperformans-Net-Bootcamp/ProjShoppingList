3a3f02f0453a4b1a9d1bb0324469ed5cb71f9215
Berengaar <emir97han@gmail.com>
2022-06-29 22:48:59 +0300

    Fluent Validation integrated to project

---
+++ src/Application/CQS/CategoryR/Commands/UpdateCategory/UpdateCategoryCommandValidator.cs
@@ -0,0 +1,39 @@
+﻿using Application.Common.Interfaces;
+using Domain.Consts.Messages.ValidationMessages;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQS.CategoryR.Commands.UpdateCategory
+{
+    public class UpdateCategoryCommandValidator : AbstractValidator<UpdateCategoryCommandRequest>
+    {
+        private readonly IProjShoppingListDbContext _context;
+        public UpdateCategoryCommandValidator(IProjShoppingListDbContext context)
+        {
+          _context = context;
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage(CategoryValidationMessages.NameNotEmpty)
+                .MaximumLength(200).MinimumLength(3).WithMessage(CategoryValidationMessages.NameMaxLength)
+                .MustAsync(UniqueName).WithMessage(CategoryValidationMessages.NameUnique);
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage(CategoryValidationMessages.DescriptionNotEmpty)
+                .MaximumLength(200).MinimumLength(3).WithMessage(CategoryValidationMessages.DescriptionMaxLength);
+        }
+
+        public async Task<bool> UniqueName(string name, CancellationToken cancellationToken)
+        {
+            var control = _context.Categories.FirstOrDefault(f => f.Name == name);
+            if (control == null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ src/Application/CQS/ProductR/Commands/AddProductToShopList/AddProductToShopListCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Interfaces;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQS.ProductR.Commands.AddProductToShopList
+{
+    public class AddProductToShopListCommandValidator:AbstractValidator<AddProductToShopListCommandRequest>
+    {
+        private readonly IProjShoppingListDbContext _context;
+        public AddProductToShopListCommandValidator()
+        {
+            RuleFor(x => x.ShopListId)
+                .NotEmpty().NotNull().WithMessage("Please enter correctly");
+            RuleFor()
+        }
+        public async Task<bool> UniqueId(string id, CancellationToken cancellationToken)
+        {
+            var control = _context.ShopLists.FirstOrDefault(f => f.Id == id);
+            if (control != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
